ea640a34ab7115ba23185981c856e25b2f8cbc75
xsennesr <xsennesr@stuba.sk>
2023-05-12 21:31:31 +0200

    nothing

--- resources/views/introduction-student/studentContent.blade.php
+++ resources/views/introduction-student/studentContent.blade.php
@@ -1,15 +1,18 @@
 <div class="mt-3 mb-5 w-100 rounded py-2 px-3" style="background-color: rgba(176,196,222,0.7); border-left: solid black 5px">
-    <h1>{{ __('introduction.s-header')  }}</h1>
-    <label>{{ __('introduction.s-intro')  }}</label>
 
-    <br><br>
-    <ol>
-        <li>{{ __('introduction.s-first')  }}</li>
-        <li>{{ __('introduction.s-second')  }}</li>
-        <li>{{ __('introduction.s-third')  }}</li>
-        <li>{{ __('introduction.s-fourth')  }}</li>
-        <li>{{ __('introduction.s-fifth')  }}</li>
-    </ol>
-    <br>
+    <h1>{{ __('introduction.s-header')  }}</h1>
+        <label>{{ __('introduction.s-intro')  }}</label>
+        <br><br>
+
+        <ol>
+            <li>{{ __('introduction.s-first')  }}</li>
+            <li>{{ __('introduction.s-second')  }}</li>
+            <li>{{ __('introduction.s-third')  }}</li>
+            <li>{{ __('introduction.s-fourth')  }}</li>
+            <li>{{ __('introduction.s-fifth')  }}</li>
+        </ol>
+        <br>
+
     {{ __('introduction.s-footer')  }}
+
 </div>
--- resources/views/introduction-teacher/teacherContent.blade.php
+++ resources/views/introduction-teacher/teacherContent.blade.php
@@ -1,31 +1,32 @@
 <div class="mt-3 mb-5 w-100 rounded py-2 px-3" style="background-color: rgba(176,196,222,0.7); border-left: solid black 5px">
 
-<h1>{{ __('introduction.t-header')  }}</h1>
-    <label>{{ __('introduction.t-intro')  }}</label>
-    <br><br>
-<h2>{{ __('introduction.t-header2')  }}</h2>
+    <h1>{{ __('introduction.t-header')  }}</h1>
+        <label>{{ __('introduction.t-intro')  }}</label>
+        <br><br>
 
-    <ol>
-        <li>{{ __('introduction.t-first')  }}</li>
-        <li>{{ __('introduction.t-second')  }}</li>
-        <li>{{ __('introduction.t-third')  }}</li>
-    </ol>
-    <br>
+    <h2>{{ __('introduction.t-header2')  }}</h2>
+        <ol>
+            <li>{{ __('introduction.t-first')  }}</li>
+            <li>{{ __('introduction.t-second')  }}</li>
+            <li>{{ __('introduction.t-third')  }}</li>
+        </ol>
+        <br>
 
-<h2>{{ __('introduction.t-header3')  }}</h2>
-    <ol>
-        <li>{{ __('introduction.t-fourth')  }}</li>
-        <li>{{ __('introduction.t-fifth')  }}</li>
-        <li>{{ __('introduction.t-sixth')  }}</li>
-    </ol>
-    <br>
+    <h2>{{ __('introduction.t-header3')  }}</h2>
+        <ol>
+            <li>{{ __('introduction.t-fourth')  }}</li>
+            <li>{{ __('introduction.t-fifth')  }}</li>
+            <li>{{ __('introduction.t-sixth')  }}</li>
+        </ol>
+        <br>
 
-<h2>{{ __('introduction.t-header4')  }}</h2>
-    <ol>
-        <li>{{ __('introduction.t-seventh')  }}</li>
-        <li>{{ __('introduction.t-eight')  }}</li>
-        <li>{{ __('introduction.t-nineth')  }}</li>
-        <li>{{ __('introduction.t-tenth')  }}</li>
-    </ol>
-    <br>
+    <h2>{{ __('introduction.t-header4')  }}</h2>
+        <ol>
+            <li>{{ __('introduction.t-seventh')  }}</li>
+            <li>{{ __('introduction.t-eight')  }}</li>
+            <li>{{ __('introduction.t-nineth')  }}</li>
+            <li>{{ __('introduction.t-tenth')  }}</li>
+        </ol>
+        <br>
+
 </div>
