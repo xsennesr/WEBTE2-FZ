812d5eb3979dfeafaeffaf66853d23052e52e66b
xsennesr <xsennesr@stuba.sk>
2023-05-12 18:26:53 +0200

    student introduction sk/en

---
+++ resources/lang/sk/introduction-student.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+
+    'header' => 'Návod na vygenerovanie príkladu',
+    'intro' => 'Nasledujúci návod opisuje postup, ako si môžte vygenerovať sadu príkladov a následne pozrieť, koľko bodov ste získali. Návod predpokladá, že už máte vytvorený účet a ste prihlásený.',
+    'first' => 'Na podstránke "Domov" v sekcii "Dostupné sady príkladov" zakliknite sadu príkladov, ktorú chcete vygenerovať',
+    'second' => 'Kliknite na "Generovať úlohu"',
+    'thirs' => 'V sekcii "Úlohy" v tabuľke môžte pozorovať priradené úlohy',
+    'fourth' => 'Kliknite na "Vyriešiť" pri tom príklade, ktorý chcete vyriešiť',
+    'fifth' => 'Vyplnte vaše rieše a kliknite na "Odovzdať"',
+    'footer' => 'Na podstránke "Domov" v sekcii "Dostupné sady príkladov" môžte následne sledovať počet dosiahnutých bodov za jednotlivé úlohy.'
+
+];
--- resources/views/introduction-student/dashboard.blade.php
+++ resources/views/introduction-student/dashboard.blade.php
@@ -2,19 +2,19 @@
 @section('content')
 
 <div class="fs-2 mt-3 mb-5 w-100 rounded py-2 px-3" style="background-color: rgba(176,196,222,0.7); border-left: solid black 5px">
-    <h2>Návod na vygenerovanie príkladu</h2>
-    Nasledujúci návod opisuje postup, ako si môžte vygenerovať sadu príkladov a následne pozrieť, koľko bodov ste získali. 
-    Návod predpokladá, že už máte vytvorený účet a ste prihlásený.
+    <h2>{{ __('introduction-student.header')  }}</h2>
+    {{ __('introduction-student.intro')  }}
+
     <br><br>
     <ol>
-        <li>Na podstránke "Domov" v sekcii "Dostupné sady príkladov" zakliknite sadu príkladov, ktorú chcete vygenerovať</li>
-        <li>Kliknite na "Generovať úlohu"</li>
-        <li>V sekcii "Úlohy" v tabuľke môžte pozorovať priradené úlohy</li>
-        <li>Kliknite na "Vyriešiť" pri tom príklade, ktorý chcete vyriešiť</li>
-        <li>Vyplnte vaše rieše a kliknite na "Odovzdať"</li>
+        <li>{{ __('introduction-student.first')  }}</li>
+        <li>{{ __('introduction-student.second')  }}</li>
+        <li>{{ __('introduction-student.third')  }}</li>
+        <li>{{ __('introduction-student.fourth')  }}</li>
+        <li>{{ __('introduction-student.fifth')  }}</li>
     </ol>
     <br>
-    Na podstránke "Domov" v sekcii "Dostupné sady príkladov" môžte následne sledovať počet dosiahnutých bodov za jednotlivé úlohy.
+    {{ __('introduction-student.footer')  }}
 </div>
 
 @endsection
